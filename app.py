f8c81602709b41f3031c4181afb67ebf0d2aa780
Hoahufi2002 <xminhhoa8@gmail.com>
2024-06-28 00:44:53 +0700

    final

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, redirect, url_for, session,request,sen
 import pyodbc
 import matplotlib.pyplot as plt
 import seaborn as sns
+import json
 
 import io
 app = Flask(__name__)
@@ -80,54 +81,10 @@ def index():
     cursor.close()
 
     return render_template('index.html', total_ingredients=total_ingredients, total_dishes=total_dishes, total_suppliers=total_suppliers, ingredient_names=ingredient_names, ingredient_stock_levels=ingredient_stock_levels, supplier_names=supplier_names, ingredients_by_supplier=ingredients_by_supplier)
-
-@app.route('/thongke', methods=['GET', 'POST'])
-def thong_ke():
-    start_date = request.args.get('start_date')
-    end_date = request.args.get('end_date')
-    data_nguyenlieu = []
-    data_phugia = []
-
-    if start_date and end_date:
-        conn = pyodbc.connect(connection_string)
-        cursor = conn.cursor()
-        query_nguyenlieu = """
-        SELECT 
-            n.TenNguyenLieu,
-            ISNULL(SUM(i.SoLuongTonKho), 0) as TotalNhap,
-            ISNULL(SUM(o.SoLuongXuat), 0) as TotalXuat
-        FROM NguyenLieuPhoBo n
-        LEFT JOIN NguyenLieuPhoBo i ON n.ID = i.ID AND i.NgayNhap BETWEEN ? AND ?
-        LEFT JOIN XuatKhoNguyenLieu o ON n.ID = o.NguyenLieuID AND o.NgayXuat BETWEEN ? AND ?
-        GROUP BY n.TenNguyenLieu
-        """
-        cursor.execute(query_nguyenlieu, (start_date, end_date, start_date, end_date))
-        data_nguyenlieu = cursor.fetchall()
-
-        query_phugia = """
-        SELECT 
-            p.TenPhuGia,
-            ISNULL(SUM(k.SoLuongTonKho), 0) as TotalNhap,
-            ISNULL(SUM(x.SoLuongXuat), 0) as TotalXuat
-        FROM PhuGiaGiaVi p
-        LEFT JOIN PhuGiaGiaVi k ON p.ID = k.ID AND k.NgayNhap BETWEEN ? AND ?
-        LEFT JOIN XuatKhoPhuGia x ON p.ID = x.PhuGiaID AND x.NgayXuat BETWEEN ? AND ?
-        GROUP BY p.TenPhuGia
-        """
-        cursor.execute(query_phugia, (start_date, end_date, start_date, end_date))
-        data_phugia = cursor.fetchall()
-
-        conn.close()
-
-    return render_template('./baocao/quanly_baocao.html', data_nguyenlieu=data_nguyenlieu, data_phugia=data_phugia, start_date=start_date, end_date=end_date)
-
-@app.route('/plot_nguyenlieu.png')
-def plot_nguyenlieu_png():
-    start_date = request.args.get('start_date')
-    end_date = request.args.get('end_date')
+def fetch_data_nguyenlieu(start_date, end_date, tennguyenlieu=None):
     conn = pyodbc.connect(connection_string)
     cursor = conn.cursor()
-    query_nguyenlieu = """
+    query = """
     SELECT 
         n.TenNguyenLieu,
         ISNULL(SUM(i.SoLuongTonKho), 0) as TotalNhap,
@@ -135,18 +92,80 @@ def plot_nguyenlieu_png():
     FROM NguyenLieuPhoBo n
     LEFT JOIN NguyenLieuPhoBo i ON n.ID = i.ID AND i.NgayNhap BETWEEN ? AND ?
     LEFT JOIN XuatKhoNguyenLieu o ON n.ID = o.NguyenLieuID AND o.NgayXuat BETWEEN ? AND ?
+    WHERE (? IS NULL OR ? = '' OR n.TenNguyenLieu = ?)
     GROUP BY n.TenNguyenLieu
     """
-    cursor.execute(query_nguyenlieu, (start_date, end_date, start_date, end_date))
-    data_nguyenlieu = cursor.fetchall()
+    cursor.execute(query, (start_date, end_date, start_date, end_date, tennguyenlieu, tennguyenlieu, tennguyenlieu))
+    data = cursor.fetchall()
+    cursor.close()
     conn.close()
+    
+    result = [{"TenNguyenLieu": row.TenNguyenLieu, "TotalNhap": row.TotalNhap, "TotalXuat": row.TotalXuat} for row in data]
+    return result
+def fetch_data_phugia(start_date, end_date, tenphugia=None):
+    conn = pyodbc.connect(connection_string)
+    cursor = conn.cursor()
+    query = """
+    SELECT 
+        p.TenPhuGia,
+        ISNULL(SUM(k.SoLuongTonKho), 0) as TotalNhap,
+        ISNULL(SUM(x.SoLuongXuat), 0) as TotalXuat
+    FROM PhuGiaGiaVi p
+    LEFT JOIN PhuGiaGiaVi k ON p.ID = k.ID AND k.NgayNhap BETWEEN ? AND ?
+    LEFT JOIN XuatKhoPhuGia x ON p.ID = x.PhuGiaID AND x.NgayXuat BETWEEN ? AND ?
+    WHERE (? IS NULL OR ? = '' OR p.TenPhuGia = ?)
+    GROUP BY p.TenPhuGia
+    """
+    cursor.execute(query, (start_date, end_date, start_date, end_date, tenphugia, tenphugia, tenphugia))
+    data = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    result = [{"TenPhuGia": row.TenPhuGia, "TotalNhap": row.TotalNhap, "TotalXuat": row.TotalXuat} for row in data]
+    return result
 
-    ten_nguyen_lieu = [row.TenNguyenLieu for row in data_nguyenlieu]
-    total_nhap = [row.TotalNhap for row in data_nguyenlieu]
-    total_xuat = [row.TotalXuat for row in data_nguyenlieu]
+@app.route('/thong_ke', methods=['GET'])
+def thong_ke():
+    start_date = request.args.get('start_date')
+    end_date = request.args.get('end_date')
+    tennguyenlieu = request.args.get('tennguyenlieu')
+    tenphugia = request.args.get('tenphugia')
+
+    data_nguyenlieu = fetch_data_nguyenlieu(start_date, end_date, tennguyenlieu)
+    data_phugia = fetch_data_phugia(start_date, end_date, tenphugia)
+
+    nguyenlieu_labels = [item['TenNguyenLieu'] for item in data_nguyenlieu] if data_nguyenlieu else []
+    nguyenlieu_data_nhap = [item['TotalNhap'] for item in data_nguyenlieu] if data_nguyenlieu else []
+    nguyenlieu_data_xuat = [item['TotalXuat'] for item in data_nguyenlieu] if data_nguyenlieu else []
+    
+    phugia_labels = [item['TenPhuGia'] for item in data_phugia] if data_phugia else []
+    phugia_data_nhap = [item['TotalNhap'] for item in data_phugia] if data_phugia else []
+    phugia_data_xuat = [item['TotalXuat'] for item in data_phugia] if data_phugia else []
+
+    return render_template('./baocao/quanly_baocao.html', 
+                           data_nguyenlieu=data_nguyenlieu, 
+                           data_phugia=data_phugia, 
+                           start_date=start_date, 
+                           end_date=end_date,
+                           nguyenlieu_labels=json.dumps(nguyenlieu_labels),
+                           nguyenlieu_data_nhap=json.dumps(nguyenlieu_data_nhap),
+                           nguyenlieu_data_xuat=json.dumps(nguyenlieu_data_xuat),
+                           phugia_labels=json.dumps(phugia_labels),
+                           phugia_data_nhap=json.dumps(phugia_data_nhap),
+                           phugia_data_xuat=json.dumps(phugia_data_xuat))
+
+@app.route('/plot_nguyenlieu.png')
+def plot_nguyenlieu_png():
+    start_date = request.args.get('start_date')
+    end_date = request.args.get('end_date')
+    tennguyenlieu = request.args.get('tennguyenlieu')
+    data_nguyenlieu = fetch_data_nguyenlieu(start_date, end_date, tennguyenlieu)
+
+    ten_nguyen_lieu = [row['TenNguyenLieu'] for row in data_nguyenlieu]
+    total_nhap = [row['TotalNhap'] for row in data_nguyenlieu]
+    total_xuat = [row['TotalXuat'] for row in data_nguyenlieu]
 
     sns.set(style="whitegrid")
-    plt.figure(figsize=(14, 8))
+    plt.figure(figsize=(10, 10))
     ax = sns.barplot(x=ten_nguyen_lieu, y=total_nhap, color='blue', label='Nhập Kho')
     sns.barplot(x=ten_nguyen_lieu, y=total_xuat, color='red', label='Xuất Kho')
 
@@ -164,28 +183,15 @@ def plot_nguyenlieu_png():
 def plot_phugia_png():
     start_date = request.args.get('start_date')
     end_date = request.args.get('end_date')
-    conn = pyodbc.connect(connection_string)
-    cursor = conn.cursor()
-    query_phugia = """
-    SELECT 
-        p.TenPhuGia,
-        ISNULL(SUM(k.SoLuongTonKho), 0) as TotalNhap,
-        ISNULL(SUM(x.SoLuongXuat), 0) as TotalXuat
-    FROM PhuGiaGiaVi p
-    LEFT JOIN PhuGiaGiaVi k ON p.ID = k.ID AND k.NgayNhap BETWEEN ? AND ?
-    LEFT JOIN XuatKhoPhuGia x ON p.ID = x.PhuGiaID AND x.NgayXuat BETWEEN ? AND ?
-    GROUP BY p.TenPhuGia
-    """
-    cursor.execute(query_phugia, (start_date, end_date, start_date, end_date))
-    data_phugia = cursor.fetchall()
-    conn.close()
+    tenphugia = request.args.get('tenphugia')
+    data_phugia = fetch_data_phugia(start_date, end_date, tenphugia)
 
-    ten_phu_gia = [row.TenPhuGia for row in data_phugia]
-    total_nhap = [row.TotalNhap for row in data_phugia]
-    total_xuat = [row.TotalXuat for row in data_phugia]
+    ten_phu_gia = [row['TenPhuGia'] for row in data_phugia]
+    total_nhap = [row['TotalNhap'] for row in data_phugia]
+    total_xuat = [row['TotalXuat'] for row in data_phugia]
 
     sns.set(style="whitegrid")
-    plt.figure(figsize=(14, 8))
+    plt.figure(figsize=(10, 10))
     ax = sns.barplot(x=ten_phu_gia, y=total_nhap, color='blue', label='Nhập Kho')
     sns.barplot(x=ten_phu_gia, y=total_xuat, color='red', label='Xuất Kho')
 
